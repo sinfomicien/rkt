a8c7d9c1e75b6bc399baf81a41f231ec3da185ce
Nicolas Blanc <sinfomicien@gmail.com>
2016-02-09 14:52:17 +0100

    Remove one level of lib

--- stage1/usr_from_host/usr_from_host.mk
+++ stage1/usr_from_host/usr_from_host.mk
@@ -6,12 +6,12 @@ $(call generate-stamp-rule,$(UFH_STAMP),,$(S1_RF_ACIROOTFSDIR), \
 	$(call vb,v2,LN SF,host $(S1_RF_ACIROOTFSDIR)/flavor) \
 	ln -sf 'host' "$(S1_RF_ACIROOTFSDIR)/flavor" && \
 	mkdir -p "$(S1_RF_ACIROOTFSDIR)/usr/lib" && \
-	ln -sf usr/lib "$(S1_RF_ACIROOTFSDIR)/lib" && \
-	ln -sf usr/lib "$(S1_RF_ACIROOTFSDIR)/lib64"&& \
-	ln -sf lib "$(S1_RF_ACIROOTFSDIR)/usr/lib64")
+	mkdir -p "$(S1_RF_ACIROOTFSDIR)/usr/lib64" && \
+	ln -sf usr/lib64 "$(S1_RF_ACIROOTFSDIR)/lib64" && \
+	ln -sf usr/lib "$(S1_RF_ACIROOTFSDIR)/lib")
 
 
-CLEAN_SYMLINKS += $(S1_RF_ACIROOTFSDIR)/flavor $(S1_RF_ACIROOTFSDIR)/lib $(S1_RF_ACIROOTFSDIR)/lib64 $(S1_RF_ACIROOTFSDIR)/usr/lib64
+CLEAN_SYMLINKS += $(S1_RF_ACIROOTFSDIR)/flavor $(S1_RF_ACIROOTFSDIR)/lib64
 
 CLEAN_DIRS += $(S1_RF_ACIROOTFSDIR)/usr/lib
 
